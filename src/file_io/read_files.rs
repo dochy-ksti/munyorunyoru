cda26f35869f3dad6f1730222ce87f53b9e6dc85
juzu <dochy.ksti@gmail.com>
2023-10-21 14:40:18 +0900

    tried cargo fix

--- src/file_io/read_files.rs
+++ src/file_io/read_files.rs
@@ -4,7 +4,7 @@ use crate::converters::converter::Converter;
 
 use super::receiver::Receiver;
 
-pub fn read_files<I, P, T>(pathes: I, converter: Converter<T>) -> Receiver<T>
+pub fn read_files<I, P, T>(pathes: I, _converter: Converter<T>) -> Receiver<T>
 where
     I: Iterator<Item = P>,
     P: AsRef<Path>,
@@ -12,9 +12,9 @@ where
     
 
     let pathes : Vec<PathBuf> = pathes.map(|p| p.as_ref().to_path_buf()).collect();
-    let (sender, receiver) = async_channel::bounded(pathes.len());
+    let (_sender, receiver) = async_channel::bounded(pathes.len());
     std::thread::spawn(move || {
-        for path in pathes.iter() {
+        for _path in pathes.iter() {
             
         }
     });
