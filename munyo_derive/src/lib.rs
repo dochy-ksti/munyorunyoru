1506611fcd0c5411581919d00ebb055d5e9103fb
juzu <dochy.ksti@gmail.com>
2023-11-20 13:21:21 +0900

    Munyo doesn't need proc-macro

--- munyo_derive/src/lib.rs
+++
@@ -1,8 +0,0 @@
-mod sample1;
-
-#[proc_macro_derive(MunyoSerialize, attributes(munyo))]
-pub fn munyo(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let item = dbg!(item);
-    //item
-    proc_macro::TokenStream::new()
-}
--- munyo_derive_test/src/main.rs
+++
@@ -1,8 +0,0 @@
-mod enu;
-mod self_serialize;
-mod derive_test;
-fn main() {
-    println!("Hello, world!");
-}
-
-
