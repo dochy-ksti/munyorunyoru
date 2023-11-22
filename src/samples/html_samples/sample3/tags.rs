12bde0370dcbe56a135fdb8b0c8fa3a16cc3bfd4
juzu <dochy.ksti@gmail.com>
2023-11-22 20:45:03 +0900

    sample3 done

--- src/samples/html_samples/sample3/tags.rs
+++ src/samples/html_samples/sample3/tags.rs
@@ -6,37 +6,49 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub enum Tags {
-	Text(RestOf),
-	BR,
-    H3(String, Vec<Tags>),
-    P(Vec<Tags>),
-    Blockquote(Vec<Tags>),
+    Alice(RestOf),
+    Bob(RestOf),
+    H3(RestOf, Class),
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Class {
+    pub class: String,
 }
 
 pub fn to_html_items(items: &[Tags]) -> Vec<HtmlItem> {
     let mut r: Vec<HtmlItem> = vec![];
     for item in items {
         match item {
-			Tags::Text(t) =>{
-				r.push(text(&t.arg));
-			}
-			Tags::BR =>{
-				r.push(tag("br", vec![], vec![]))
-			}
-            Tags::H3(c, tags) => {
-                r.push(tag("h3", class(c), to_html_items(tags)));
+            Tags::Alice(t) => {
+                balloon(true, &t.arg, &mut r);
             }
-            Tags::P(tags) => {
-                r.push(tag("p", vec![], to_html_items(tags)));
+            Tags::Bob(t) => {
+                balloon(false, &t.arg, &mut r);
             }
-            Tags::Blockquote(v) => r.push(tag("blockquote", vec![], to_html_items(v))),
+            Tags::H3(t, c) => {
+                r.push(tag("h3", class(c), vec![text(&t.arg)]));
+            }
         }
     }
     r
 }
 
-fn text(s: &str) -> HtmlItem {
-    HtmlItem::Text(s.to_string())
+fn balloon(is_l: bool, text: &str, r: &mut Vec<HtmlItem>) {
+    let bl = if is_l { "balloonL" } else { "balloonR" };
+    let pict = if is_l { "girl.png" } else { "boy.png" };
+    let speaker = if is_l { "Alice" } else { "Bob" };
+    let t = format!(
+        r###"
+<div class="balloon {}">
+  <div class="balloon-img"><figure><img src="{}" /><figcaption>{}</figcaption></figure></div>
+  <div class="balloon-text"><div class="balloon-text-inner">
+  {}
+  </div></div>
+</div>"###,
+        bl, pict, speaker, text
+    );
+    r.push(self::text(&t))
 }
 
 fn tag(name: &str, params: Vec<Param>, children: Vec<HtmlItem>) -> HtmlItem {
@@ -49,10 +61,13 @@ fn tag(name: &str, params: Vec<Param>, children: Vec<HtmlItem>) -> HtmlItem {
     )
 }
 
-fn class(class: &str) -> Vec<Param> {
-    vec![Param {
-        name: "class".to_string(),
-        value: class.to_string(),
-    }]
+fn text(s: &str) -> HtmlItem {
+    HtmlItem::Text(s.to_string())
 }
 
+fn class(class: &Class) -> Vec<Param> {
+    vec![Param {
+        name: "class".to_string(),
+        value: class.class.clone(),
+    }]
+}
